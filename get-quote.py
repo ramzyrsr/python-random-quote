9433dcdff1773a9318ceaea57d2c6a88ec934c5e
ramzyrsr <79071247+ramzyrsr@users.noreply.github.com>
2021-02-15 21:14:38 +0700

    Update get-quote.py
    get quote

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 15 21:06:12 2021
+
+@author: Zyrex
+"""
 def primary():
   print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-  #print(quotes)
+  print(quotes[0])
 
 if __name__== "__main__":
   primary()
